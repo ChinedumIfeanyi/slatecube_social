001762712a8107c3be3a7b0d3226c4bba03d50f4
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-02 19:17:58 +0100

    chore(project setup FE) setup frontend build process

---
+++ webpack.client.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+	name: 'browser',
+	mode: 'development',
+	entry: './client/main.js',
+	devtool: 'cheap-eval-source-map',
+	resolve: {
+		extensions: ['.js']
+	},
+	output: {
+		path: path.resolve(__dirname, './client/dist'),
+		filename: 'clientBundle.js',
+		publicPath: '/'
+	},
+	module:{
+		rules: [
+			{
+				test: /\.js$/,
+				use: ['babel-loader'],
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: './client/index.html',
+			inject: true
+		})
+	],
+	devServer:{
+		"proxy": {
+		  "**": "http://[::1]:7000"
+		},
+		hot: true,
+		historyApiFallback: true,
+		inline: true,
+		contentBase: path.resolve(__dirname, "./client/dist"),
+		watchOptions: {
+			ignored: /node_modules/,
+			poll: true
+		}
+	},
+}
