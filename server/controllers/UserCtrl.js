791d566d04855daf6010ac0428ee3fd5c6d99bfe
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-07 00:18:44 +0100

    feat(server): implement route for user auth

---
+++ server/controllers/UserCtrl.js
@@ -0,0 +1,15 @@
+
+
+class UserCtrl {
+	
+	static login() {
+
+	}
+
+	static register() {
+
+	}
+
+}
+
+export default UserCtrl
--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
-import app from './main'
+import app from './app'
+
+import Route from "./routes"
+
 
 const PORT = process.env.PORT || 7000
+
+
+
+//User auth 
+app.use('/auth', Route.userRoute)
+
+
+
+
+
 
 app.listen(PORT, ()=> console.log(`server booted @ ${PORT}`))
